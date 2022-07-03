d022a51d8a5c90769823aea617fdb975e3136fe8
Piotr Rudnicki <deamondev@gmail.com>
2022-07-03 20:51:31 +0200

    test corrected

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import Test.HUnit
-import qualified ParserSpec 
+import qualified ParserSpec
+import qualified InterpreterSpec
 --import qualified InterpreterSpec
 import Control.Monad
 
 main :: IO ()
-main = void $ runTestTT ParserSpec.tests
+main = do
+  void $ runTestTT ParserSpec.tests
+  void $ runTestTT InterpreterSpec.tests
--- test/interpreter/InterpreterSpec.hs
+++ test/interpreter/InterpreterSpec.hs
@@ -2,9 +2,10 @@ module InterpreterSpec where
 import Test.HUnit.Base (Test (TestCase), assertEqual)
 import Parser (parse)
 import Interpreter (execute)
+import Test.HUnit
 
 flags1 :: [String]
-flags1 = ["--generate", "--token"]
+flags1 = ["--generate", "--token", "--generate", "--easteregg"]
 
 test1 :: Test
 test1 = TestCase $ do 
@@ -12,3 +13,6 @@ test1 = TestCase $ do
                         expected = "Generated token 17 And Generated easter egg: cats are fun"
                     result <- execute expr 
                     assertEqual "flag1" (Right expected) result
+
+tests :: Test
+tests = TestList [test1]
